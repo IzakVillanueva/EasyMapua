6d6f8e8590301ab2360b72eee73943c93a004664
IzakVillanueva <82789169+IzakVillanueva@users.noreply.github.com>
2023-01-25 18:57:21 +0800

    Add register activity

---
+++ app/src/main/java/com/example/easymapua/Register.java
@@ -0,0 +1,33 @@
+package com.example.easymapua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Register extends AppCompatActivity {
+    private Button buttonSign;
+    private EditText user;
+    private EditText password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        user = (EditText) findViewById(R.id.loginEdit);
+        password = (EditText) findViewById(R.id.editTextPassword);
+        String sUser = user.getText().toString();
+        String sPassword = user.getText().toString();
+
+        buttonSign = (Button) findViewById(R.id.signIn);
+
+        if(!sUser.matches("") && !sPassword.matches("")){
+            buttonSign.setEnabled(true);
+        }
+    }
+}
