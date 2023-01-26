9a6a2088645a0a4d289e2574a4b6bd558de037f3
IzakVillanueva <82789169+IzakVillanueva@users.noreply.github.com>
2023-01-26 18:02:55 +0800

    Add register and login system

--- app/src/main/java/com/example/easymapua/Login.java
+++ app/src/main/java/com/example/easymapua/Login.java
@@ -3,17 +3,24 @@ package com.example.easymapua;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class Login extends AppCompatActivity {
     private Button buttonLogIn;
-    private EditText user;
-    private EditText password;
+    private EditText userEdit;
+    private EditText passwordEdit;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +28,54 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         buttonLogIn = (Button) findViewById(R.id.signIn);
+        userEdit = findViewById(R.id.loginEdit);
+        passwordEdit = findViewById(R.id.editTextPassword);
+        progressBar = findViewById(R.id.progress);
 
         buttonLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String username = String.valueOf(userEdit.getText());
+                String password = String.valueOf(passwordEdit.getText());
 
+                if(!username.equals("") && !password.equals("")) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    //Start ProgressBar first (Set visibility VISIBLE)
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[2];
+                            field[0] = "username";
+                            field[1] = "password";
+                            //Creating array for data
+                            String[] data = new String[2];
+                            data[0] = username;
+                            data[1] = password;
+                            PutData putData = new PutData("http://192.168.1.7/LoginRegister/login.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    if(result.equals("Login Success")){ //==========================================add if for classification
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), Login.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "All fields required", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
