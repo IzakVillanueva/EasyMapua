c676fd3f3e9f0e9afac9d6d4d9399a0d3a0d31ac
IzakVillanueva <82789169+IzakVillanueva@users.noreply.github.com>
2023-02-01 22:19:08 +0800

    Add button function to view menu

--- app/src/main/java/com/example/easymapua/StudentNav.java
+++ app/src/main/java/com/example/easymapua/StudentNav.java
@@ -2,13 +2,26 @@ package com.example.easymapua;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StudentNav extends AppCompatActivity {
+    private Button menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_nav);
+
+        menu = findViewById(R.id.buttonShowMenu);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StudentNav.this, ViewMenu.class));
+            }
+        });
     }
 }
