478886878d1f87e8d47ad480e3fed83a369945ef
IzakVillanueva <82789169+IzakVillanueva@users.noreply.github.com>
2023-02-01 15:05:51 +0800

    Add activity for adding food and rating

---
+++ app/src/main/java/com/example/easymapua/AddRate.java
@@ -0,0 +1,14 @@
+package com.example.easymapua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddRate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_rate);
+    }
+}
