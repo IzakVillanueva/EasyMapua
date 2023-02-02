8240eac3316bb9918e96cdd7c352e4404e46882a
IzakVillanueva <82789169+IzakVillanueva@users.noreply.github.com>
2023-02-03 01:20:25 +0800

    Update prof UI

--- app/src/main/java/com/example/easymapua/ProfessorNav.java
+++ app/src/main/java/com/example/easymapua/ProfessorNav.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class ProfessorNav extends AppCompatActivity {
     private Button add, rate;
+    private ImageView ex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,14 @@ public class ProfessorNav extends AppCompatActivity {
 
         add = findViewById(R.id.buttonAdd);
         rate = findViewById(R.id.buttonRate);
+        ex = findViewById(R.id.imageViewExit);
+
+        ex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
