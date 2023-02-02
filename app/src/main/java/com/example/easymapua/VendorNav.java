395867fa2b5277daaa83b4635f17a04c4ba94d50
IzakVillanueva <82789169+IzakVillanueva@users.noreply.github.com>
2023-02-03 01:28:28 +0800

    Update vendor UI logout

--- app/src/main/java/com/example/easymapua/VendorNav.java
+++ app/src/main/java/com/example/easymapua/VendorNav.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class VendorNav extends AppCompatActivity {
     private Button add, viewRate;
+    private ImageView ex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,14 @@ public class VendorNav extends AppCompatActivity {
 
         add = findViewById(R.id.buttonAddFood);
         viewRate = findViewById(R.id.buttonViewRate);
+        ex = findViewById(R.id.imageViewExit);
+
+        ex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
