37e785b3b9721f68834e466e93a624d970e12863
IzakVillanueva <82789169+IzakVillanueva@users.noreply.github.com>
2023-04-16 17:56:06 +0800

    Update register page

--- app/src/main/java/com/example/easymapua/EmailRegister.java
+++ app/src/main/java/com/example/easymapua/EmailRegister.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -91,5 +92,19 @@ public class EmailRegister extends AppCompatActivity {
             });
 
         }
+    }
+
+    public void buttonOpenFile1(View view){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Downloads.EXTERNAL_CONTENT_URI);
+        intent.setType("*/*");
+        this.startActivity(intent);
+
+    }
+
+    public void buttonOpenFile2(View view){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Downloads.EXTERNAL_CONTENT_URI);
+        intent.setType("*/*");
+        this.startActivity(intent);
+
     }
 }
