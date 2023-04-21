692ba9a833b2d550e77131b0c06d3aec6d114b07
IzakVillanueva <82789169+IzakVillanueva@users.noreply.github.com>
2023-04-21 19:11:46 +0800

    Update Food structure

--- app/src/main/java/com/example/easymapua/Food.java
+++ app/src/main/java/com/example/easymapua/Food.java
@@ -1,4 +1,27 @@
 package com.example.easymapua;
 
 public class Food {
+    private String store, food, price;
+
+    public Food(){
+
+    }
+
+    public Food(String store, String food, String price) {
+        this.store = store;
+        this.food = food;
+        this.price = price;
+    }
+
+    public String getStore() {
+        return store;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public String getPrice() {
+        return price;
+    }
 }
