5e05998a694b464486c74a766c858a68c7df4e35
IzakVillanueva <82789169+IzakVillanueva@users.noreply.github.com>
2023-04-21 19:12:11 +0800

    Set connection to database in addfood

--- app/src/main/java/com/example/easymapua/AddFood.java
+++ app/src/main/java/com/example/easymapua/AddFood.java
@@ -13,12 +13,24 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class AddFood extends AppCompatActivity {
     private Button add, cancel;
     private EditText storeT, foodT, priceT;
     ProgressBar prog;
+    DatabaseReference databaseReference;
+    long maxIDFood;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +43,11 @@ public class AddFood extends AppCompatActivity {
         foodT = findViewById(R.id.editTextFood);
         priceT = findViewById(R.id.editTextPrice);
         prog = findViewById(R.id.progressBarFood);
+        databaseReference = FirebaseDatabase.getInstance("https://easymapuaauth-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference().child("Menu");
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String store = String.valueOf(storeT.getText());
-                String food = String.valueOf(foodT.getText());
-                String price = String.valueOf(priceT.getText());
-
                 /*if(!store.equals("") && !food.equals("") && !price.equals("")){
                     prog.setVisibility(View.VISIBLE);
                     Handler handler = new Handler(Looper.getMainLooper());
@@ -81,6 +90,8 @@ public class AddFood extends AppCompatActivity {
                 else{
                     Toast.makeText(getApplicationContext(), "All fields required", Toast.LENGTH_SHORT).show();
                 }*/
+
+                addFoodToMenu();
             }
         });
 
@@ -95,5 +106,58 @@ public class AddFood extends AppCompatActivity {
                 }
             }
         });
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    maxIDFood = (snapshot.getChildrenCount());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void addFoodToMenu() {
+        String store = String.valueOf(storeT.getText());
+        String food = String.valueOf(foodT.getText());
+        String price = String.valueOf(priceT.getText());
+
+        if(store.isEmpty()){
+            storeT.setError("Please add a store!");
+        }
+        else if(food.isEmpty()){
+            foodT.setError("Please add a food!");
+        }
+        else if(price.isEmpty()){
+            priceT.setError("Please add a price!");
+        }
+        else{
+            prog.setVisibility(View.VISIBLE);
+            Food inputFood = new Food(
+                    store,
+                    food,
+                    price
+            );
+
+            databaseReference.child(String.valueOf(maxIDFood+1))
+                    .setValue(inputFood).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(Task<Void> task) {
+                    prog.setVisibility(View.GONE);
+                    if(task.isSuccessful()){
+                        Intent intent = new Intent(getApplicationContext(), StudentNav.class);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        Toast.makeText(AddFood.this, "Added Food to Menu", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }
+            });
+        }
     }
 }
