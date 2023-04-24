afe0e699ef449a5151a5c8693ae1cd32531123d2
IzakVillanueva <82789169+IzakVillanueva@users.noreply.github.com>
2023-04-24 12:07:30 +0800

    Update view consultation sched in view sched

--- app/src/main/java/com/example/easymapua/ViewSchedule.java
+++ app/src/main/java/com/example/easymapua/ViewSchedule.java
@@ -128,72 +128,72 @@ public class ViewSchedule extends AppCompatActivity {
                 handler.post(new Runnable() {
                     @Override
                     public void run() {
+                        Query query = databaseReference.orderByChild("task").equalTo("Consultation");
+                        query.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot snapshot) {
+                                if(snapshot.exists()){
+                                    for(DataSnapshot schedDataSnap : snapshot.getChildren()){
+                                        //Extract the sched object from snapshot
+                                        Schedule sched = schedDataSnap.getValue(Schedule.class);
+                                        try{
+                                            //populate table (same code as above)
+                                            TableRow row1 = new TableRow(ViewSchedule.this);
 
-                        /*String[] field = new String[1];
-                        field[0] = "task";
-                        String[] data = new String[1];
-                        data[0] = "Consultation";
-                        PutData putData = new PutData("http://192.168.11.156/LoginRegister/viewConsultation.php", "POST", field, data);
-                        if(putData.startPut()){
-                            if(putData.onComplete()){
-                                String result = putData.getResult();
-                                JSONArray jsonArray;
-                                clearTable();
-                                try {
-                                    jsonArray = new JSONArray(result.toString());
-                                    for(int i = 0; i < jsonArray.length(); i++){
-                                        JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                        //populate table
-                                        TableRow row1 = new TableRow(ViewSchedule.this);
+                                            TextView line1 = new TextView(ViewSchedule.this);
+                                            line1.setTextAppearance(getApplicationContext(), R.style.userRateTbl);
+                                            line1.setText(sched.getProfessor() +
+                                                    " (" + sched.getCoursecode() + ")");
+                                            line1.setSingleLine(false);
+                                            line1.setWidth(TableRow.LayoutParams.WRAP_CONTENT);
+                                            row1.addView(line1);
 
-                                        TextView line1 = new TextView(ViewSchedule.this);
-                                        line1.setTextAppearance(getApplicationContext(), R.style.userRateTbl);
-                                        line1.setText(jsonObject.getString("professor") +
-                                                " (" + jsonObject.getString("coursecode") + ")");
-                                        line1.setSingleLine(false);
-                                        line1.setWidth(TableRow.LayoutParams.WRAP_CONTENT);
-                                        row1.addView(line1);
+                                            tbl.addView(row1);
 
-                                        tbl.addView(row1);
+                                            TableRow row2 = new TableRow(ViewSchedule.this);
 
-                                        TableRow row2 = new TableRow(ViewSchedule.this);
+                                            TextView line2 = new TextView(ViewSchedule.this);
+                                            line2.setTextAppearance(getApplicationContext(), R.style.subRateTbl);
+                                            line2.setText("Task: " + sched.getTask() +
+                                                    "   Room: " + sched.getRoom());
+                                            line2.setSingleLine(false);
+                                            line2.setWidth(TableRow.LayoutParams.WRAP_CONTENT);
+                                            row2.addView(line2);
 
-                                        TextView line2 = new TextView(ViewSchedule.this);
-                                        line2.setTextAppearance(getApplicationContext(), R.style.subRateTbl);
-                                        line2.setText("Task: " + jsonObject.getString("task") +
-                                                "   Room: " + jsonObject.getString("room"));
-                                        line2.setSingleLine(false);
-                                        line2.setWidth(TableRow.LayoutParams.WRAP_CONTENT);
-                                        row2.addView(line2);
+                                            tbl.addView(row2);
 
-                                        tbl.addView(row2);
+                                            TableRow row3 = new TableRow(ViewSchedule.this);
 
-                                        TableRow row3 = new TableRow(ViewSchedule.this);
+                                            TextView line3 = new TextView(ViewSchedule.this);
+                                            line3.setTextAppearance(getApplicationContext(), R.style.msgRateTbl);
+                                            line3.setText(sched.getDate() + " - " +
+                                                    sched.getStartTime() + " - " +
+                                                    sched.getEndTime());
+                                            line3.setSingleLine(false);
+                                            line3.setWidth(TableRow.LayoutParams.WRAP_CONTENT);
+                                            row3.addView(line3);
 
-                                        TextView line3 = new TextView(ViewSchedule.this);
-                                        line3.setTextAppearance(getApplicationContext(), R.style.msgRateTbl);
-                                        line3.setText(jsonObject.getString("date") + " - " +
-                                                jsonObject.getString("starttime") + " - " +
-                                                jsonObject.getString("endtime"));
-                                        line3.setSingleLine(false);
-                                        line3.setWidth(TableRow.LayoutParams.WRAP_CONTENT);
-                                        row3.addView(line3);
+                                            tbl.addView(row3);
 
-                                        tbl.addView(row3);
+                                            TableRow row4 = new TableRow(ViewSchedule.this);
+                                            TextView blank = new TextView(ViewSchedule.this);
+                                            blank.setText("");
+                                            row4.addView(blank);
 
-                                        TableRow row4 = new TableRow(ViewSchedule.this);
-                                        TextView blank = new TextView(ViewSchedule.this);
-                                        blank.setText("");
-                                        row4.addView(blank);
-
-                                        tbl.addView(row4);
-                                        prog.setVisibility(View.GONE);
+                                            tbl.addView(row4);
+                                            prog.setVisibility(View.GONE);
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                        }
                                     }
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
                                 }
                             }
-                        }*/
+
+                            @Override
+                            public void onCancelled(DatabaseError error) {
+                                Toast.makeText(ViewSchedule.this, "Query cancelled", Toast.LENGTH_SHORT).show();
+                            }
+                        });
                     }
                 });
             }
