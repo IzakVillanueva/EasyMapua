7cbdd8b296f9ce50c1efc823aa7ff9e9bb66f0ae
IzakVillanueva <82789169+IzakVillanueva@users.noreply.github.com>
2023-04-24 14:24:46 +0800

    Update Rating.java add rate value

--- app/src/main/java/com/example/easymapua/Rating.java
+++ app/src/main/java/com/example/easymapua/Rating.java
@@ -1,4 +1,42 @@
 package com.example.easymapua;
 
 public class Rating {
+    private String username, classification, store, subject, message, rate;
+
+    public Rating(){
+
+    }
+
+    public Rating(String username, String classification, String store, String subject, String message, String rate) {
+        this.username = username;
+        this.classification = classification;
+        this.store = store;
+        this.subject = subject;
+        this.message = message;
+        this.rate = rate;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getClassification() {
+        return classification;
+    }
+
+    public String getStore() {
+        return store;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getRate() {
+        return rate;
+    }
 }
